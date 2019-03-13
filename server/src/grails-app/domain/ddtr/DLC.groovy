be767fbbcb20257b377f6954892e9ef13101bd93
Jerry U <jerry@lyshnia.com>
2019-03-14 01:54:08 +0800

    Created Operator module

---
+++ server/src/grails-app/domain/ddtr/DLC.groovy
@@ -0,0 +1,14 @@
+package ddtr
+
+class DLC {
+    String name
+    String state
+    String lga
+
+    static constraints = {
+    }
+
+    String toString() {
+        this.name
+    }
+}
--- server/src/grails-app/init/ddtr/BootStrap.groovy
+++ server/src/grails-app/init/ddtr/BootStrap.groovy
@@ -3,16 +3,24 @@ package ddtr
 class BootStrap {
 
     def init = { servletContext ->
-        User user = new User(username: "08025254356", password: "pass")
+        DLC dlc = new DLC(name: "FCT_DLC", state: "Abuja", lga: "Mabushi")
+        dlc.save()
+        User user = new User(username: "08025254356", password: "pass", first: "Jerry", last: "U.", dlc: dlc)
         user.save()
 
         Authority payment = new Authority(authority: "ROLE_PAYMENT")
         Authority ref = new Authority(authority: "ROLE_REF")
+        Authority admin = new Authority(authority: "ROLE_ADMIN")
+        Authority capture = new Authority(authority: "ROLE_CAPTURE")
 
         ref.save()
+        admin.save()
+        capture.save()
         payment.save()
 
         UserAuthority.create(user, ref)
+        UserAuthority.create(user, admin)
+        UserAuthority.create(user, capture)
         UserAuthority.create(user, payment)
     }
     def destroy = {
